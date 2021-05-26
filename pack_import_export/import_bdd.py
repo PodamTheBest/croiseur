fb3a197e2830190a918555dc145d88998ccb2bf6
Neldarion <guillaume-le-du@hotmail.fr>
2021-05-26 15:23:32 +0200

    croiseur v0.3

--- pack_import_export/import_bdd.py
+++ pack_import_export/import_bdd.py
@@ -1,6 +1,6 @@
 import mysql.connector
 conn = mysql.connector.connect(host="192.168.125.2",
-                               user="FN-2199", password="1_h473_7R@i70Rs",
+                               user="FN-2199", password="***************",
                                database="stormtroopers")
 cursor = conn.cursor()
 # Opérations à réaliser sur la base ...
